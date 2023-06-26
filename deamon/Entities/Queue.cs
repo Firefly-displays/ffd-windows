579196965bb0d5b57b89cccc5c16c48a70d98ac9
oneren37-1 <oneren38@gmail.com>
2023-06-26 16:11:01 +0300

    фикс чертового импорта

--- deamon/Entities/Queue.cs
+++ deamon/Entities/Queue.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text.Json.Serialization;
-using System.Windows.Documents;
+using Newtonsoft.Json;
 
 namespace deamon.Models;
 
