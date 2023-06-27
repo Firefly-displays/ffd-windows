1339cc1fdfd0cab31ede1774769d90887c447757
oneren37-1 <oneren38@gmail.com>
2023-06-27 19:54:58 +0300

    добавил SchedulerEntity

---
+++ deamon/Entities/QueueTriggerPair.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using deamon.Models;
+using Newtonsoft.Json;
+
+namespace deamon.Entities;
+
+public class QueueTriggerPair : INotifyPropertyChanged
+{
+    public Queue Queue { get => _Queue; set => SetField(ref _Queue, value); }
+    private Queue _Queue;
+    
+    public string? Id { get => _Id; set => SetField(ref _Id, value); }
+    private string? _Id;
+    
+    public string? Cron { get => _Cron; set => SetField(ref _Cron, value); }
+    private string? _Cron;
+    
+    public DateTime? EmitTime { get => _EmitTime; set => SetField(ref _EmitTime, value); }
+    private DateTime? _EmitTime;
+    
+    public int Duration { get => _Duration; set => SetField(ref _Duration, value); }
+    private int _Duration;
+    
+    public int Priority { get => _Priority; set => SetField(ref _Priority, value); }
+    private int _Priority;
+
+    public QueueTriggerPair(Queue queue, string? cron, DateTime? emitTime, int duration, int priority)
+        : this(null, queue, cron, emitTime, duration, priority) { }
+
+    [JsonConstructor]
+    public QueueTriggerPair(string? id, Queue queue, string? cron, DateTime? emitTime, int duration, int priority)
+    {
+        Id = id ?? Guid.NewGuid().ToString();
+        Queue = queue;
+        Cron = cron;
+        EmitTime = emitTime;
+        Duration = duration;
+        Priority = priority;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
--- deamon/Entities/SchedulerEntity.cs
+++ deamon/Entities/SchedulerEntity.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 using deamon.Models;
 using Quartz;
 using Quartz.Xml.JobSchedulingData20;
@@ -9,29 +13,57 @@ namespace deamon.Entities;
 
 public class SchedulerEntity : Entity
 {
-    private string _Name;
-    private List<QueueTriggerPair> _QueueTriggerPairs;
-
     public string Name { get => _Name; set => SetField(ref _Name, value); }
+    private string _Name;
+    
+    public Queue DefaultQueue { get => _DefautlQueue; set => SetField(ref _DefautlQueue, value); }
+    private Queue _DefautlQueue;
+    
     public List<QueueTriggerPair> QueueTriggerPairs
     {
-        get => _QueueTriggerPairs; set => SetField(ref _QueueTriggerPairs, value);
+        get => _QueueTriggerPairs.ToList(); 
+        set => SetField(ref _QueueTriggerPairs,  new ObservableCollection<QueueTriggerPair>(value));
+    }
+
+    private ObservableCollection<QueueTriggerPair> _QueueTriggerPairs;
+    
+    public void Add(QueueTriggerPair pair)
+    {
+        QueueTriggerPairs.Add(pair);
     }
     
-    
-    // public SchedulerEntity(string name)
-    // {
-    //     this.Name = name;
-    //     this.QueueTriggerPairs = new List<QueueTriggerPair>();
-    // }
+    public SchedulerEntity(string name) : this(null, name) {}
 
-    public SchedulerEntity(string name, List<QueueTriggerPair> queueTriggerPairs)
-        : this(null, name, queueTriggerPairs) { }
-
-    [JsonConstructor]
-    public SchedulerEntity(string? id, string name, List<QueueTriggerPair> queueTriggerPairs): base(id)
+    public SchedulerEntity(string? id, string name) : base(id)
     {
+        QueueTriggerPairs = new List<QueueTriggerPair>();
         Name = name;
+
+        _QueueTriggerPairs.CollectionChanged += (sender, args) =>
+        {
+            OnPropertyChanged(nameof(QueueTriggerPairs));
+        };
+    }
+    
+    public SchedulerEntity(string name, List<QueueTriggerPair> queueTriggerPairs) : this(null, name, queueTriggerPairs) {}
+    
+    [JsonConstructor]
+    public SchedulerEntity(string? id, string name, List<QueueTriggerPair> queueTriggerPairs) : base(id)
+    {
         QueueTriggerPairs = queueTriggerPairs;
+        Name = name;
+        
+        _QueueTriggerPairs.CollectionChanged += (sender, args) =>
+        {
+            OnPropertyChanged(nameof(QueueTriggerPairs));
+            
+            foreach (var queueTriggerPair in QueueTriggerPairs)
+            {
+                queueTriggerPair.PropertyChanged += (sender, args) =>
+                {
+                    OnPropertyChanged(nameof(QueueTriggerPairs));
+                };
+            }
+        };
     }
 }
