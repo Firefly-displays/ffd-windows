9450a6b6e3619e46ac7df3fa08c97d8e29a6aa67
oneren37-1 <oneren38@gmail.com>
2023-06-27 20:35:48 +0300

    разнес RemoteClient

---
+++ deamon/Controllers/RemoteClient/RemoteClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using deamon.Models;
+using Microsoft.MixedReality.WebRTC;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+
+namespace deamon;
+
+public partial class RemoteClient
+{
+    private WebSocket ws = new("ws://localhost:6969");
+    private PeerConnection pc = new();
+    private DeamonAPI deamonApi;
+    private string saveFilePath;
+    private FileStream saveFileStream;
+
+    public RemoteClient(DeamonAPI api)
+    {
+        deamonApi = api;
+        
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.WriteLine("Соединение ws установлено");
+            ws.Send("{\"type\":\"connection\",\"role\":\"host\",\"hostID\":\"123\",\"hostPassword\":\"123\"}");
+        };
+
+        ws.OnClose += (sender, e) => { Debug.WriteLine("Соединение ws закрыто"); };
+
+        ws.OnError += (sender, e) =>
+        {
+            Debug.WriteLine("Ошибка ws: " + e.Message);
+        };
+
+        ws.OnMessage += async (sender, e) =>
+        {
+            var msg = e.Data;
+
+            Debug.WriteLine($"web socket recv: {msg.Length} bytes");
+            JObject jsonMsg = JObject.Parse(msg);
+
+            switch ((string)jsonMsg["type"]!)
+            {
+                case "get": Get(jsonMsg); break;
+                case "iceCandidate": HandleIceCandidate(jsonMsg); break;
+                case "offer": HandleOffer(jsonMsg); break;
+            }
+        };
+        
+        ws.Connect();
+    }
+
+    private void WSSend(string msg)
+    {
+        ws.Send(new JObject()
+        {
+            { "role", "host" },
+            { "hostID", "123" },
+            { "hostPassword", "123" },
+            { "message", msg }
+        }.ToString());
+    }
+}
---
+++ deamon/Controllers/RemoteClient/RemoteClientGet.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using deamon.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace deamon;
+
+public partial class RemoteClient
+{
+    private void Get(JObject jsonMsg)
+    {
+        string entity = (string)jsonMsg["entity"]!;
+        string id = (string)jsonMsg["id"]!;
+        string? requestId = (string)jsonMsg["requestId"];
+
+        List<JObject> result = new List<JObject>();
+
+        switch (entity)
+        {
+            case "display":
+                var displays = id == "*" 
+                    ? deamonApi.GET<Display>() 
+                    : new List<Display>() { deamonApi.GET<Display>(id) };
+
+                foreach (var display in displays)
+                {
+                    result.Add(new()
+                    {
+                        { "id", display.Id },
+                        { "name", display.Name },
+                        { "scheduler", display.SchedulerEntityId }
+                    });      
+                }
+                
+                break;
+            case "content":
+                var contents = id == "*" 
+                    ? deamonApi.GET<Content>() 
+                    : new List<Content>() { deamonApi.GET<Content>(id) };
+                
+                foreach (var content in contents)
+                {
+                    result.Add(new()
+                    {
+                        { "id", content.Id },
+                        { "name", content.Name },
+                        { "img", content.GetBaseThumb() }
+                    }); 
+                }
+                break;
+            case "queue":
+                if (id == "*")
+                {
+                    var queues = deamonApi.GET<Queue>();
+                    Debug.WriteLine("fetching queues");
+                    Debug.WriteLine(queues.Count);
+                    foreach (var queue in queues)
+                    {
+                        result.Add(new()
+                        {
+                            { "id", queue.Id },
+                            { "name", queue.Name }
+                        }); 
+                    }
+                }
+                else
+                {
+                    var queue = deamonApi.GET<Queue>(id);
+                    var items = queue.ContentList.Select((c, i) =>
+                    {
+                        return new JObject()
+                        {
+                            {
+                                "media", new JObject()
+                                {
+                                    { "id", c.Id },
+                                    { "name", c.Name },
+                                    { "img", c.GetBaseThumb() }
+                                }
+                            },
+                            { "priority", i }
+                        };
+                    });
+                    result.Add(new()
+                    {
+                        { "id", queue.Id },
+                        { "name", queue.Name },
+                        { "items", JsonConvert.SerializeObject(items) }
+                    });
+                }
+                break;
+            // case "schedulerEntity": t = typeof(SchedulerEntity); break;
+            default: return;
+        }
+        
+        WSSend(new JObject()
+        {
+            { "type", "response" },
+            { "requestId", requestId },
+            { "payload", JsonConvert.SerializeObject(result) }
+        }.ToString());
+    }
+}
